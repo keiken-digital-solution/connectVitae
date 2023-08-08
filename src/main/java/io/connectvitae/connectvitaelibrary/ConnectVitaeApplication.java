b6c8e3b0ace10180d869b57c3c4a1d04bc968dfb
Useph El <el@useph.me>
2023-08-08 15:14:31 +0100

    feat: generate the base project from start.spring.io

---
+++ src/main/java/io/connectvitae/connectvitaelibrary/ConnectVitaeApplication.java
@@ -0,0 +1,13 @@
+package io.connectvitae.connectvitaelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectVitaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectVitaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/connectvitae/connectvitaelibrary/ConnectVitaeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.connectvitae.connectvitaelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectVitaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
