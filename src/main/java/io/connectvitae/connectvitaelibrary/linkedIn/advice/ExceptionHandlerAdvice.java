670cdcd2055174be7624be6b93534cc4cd65734a
Useph El <el@useph.me>
2023-08-15 15:19:59 +0000

    feat(LinkedIn): implement error handling for Voyager API

---
+++ src/main/java/io/connectvitae/connectvitaelibrary/linkedIn/advice/ExceptionHandlerAdvice.java
@@ -0,0 +1,37 @@
+package io.connectvitae.connectvitaelibrary.linkedIn.advice;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.FeignException;
+import io.connectvitae.connectvitaelibrary.linkedIn.dtos.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    @ExceptionHandler(FeignException.class)
+    public ErrorDetails handleFeignException(FeignException ex) {
+        if (ex.status() == 401) {
+            return new ErrorDetails(
+                    HttpStatus.SERVICE_UNAVAILABLE,
+                    "We can't handle your request right now. Please try again later."
+            );
+        } else if (ex.status() == 403) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                JsonNode jsonNode = objectMapper.readTree(ex.contentUTF8());
+                return new ErrorDetails(
+                        HttpStatus.BAD_REQUEST,
+                        jsonNode.get("message").asText()
+                );
+            } catch (JsonProcessingException ignored) {
+            }
+        }
+        return new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/main/java/io/connectvitae/connectvitaelibrary/linkedIn/dtos/SubErrorDetails.java
@@ -0,0 +1,15 @@
+package io.connectvitae.connectvitaelibrary.linkedIn.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+public class SubErrorDetails {
+  private String field;
+  private Object rejectedValue;
+  private String message;
+}
+
