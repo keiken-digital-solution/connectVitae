9c3be65c99bcb14af7967f320584e0765957ec65
jbadr28 <63529668+jbadr28@users.noreply.github.com>
2024-06-27 21:35:17 +0100

    [Feature]: APIs documentation using SWAGGER

--- connectvitae-core/src/main/java/io/connectvitae/core/controllers/SeleniumController.java
+++ connectvitae-core/src/main/java/io/connectvitae/core/controllers/SeleniumController.java
@@ -1,21 +1,15 @@
 package io.connectvitae.core.controllers;
 
-import io.connectvitae.contracts.models.Certification;
-import io.connectvitae.contracts.models.Education;
-import io.connectvitae.contracts.models.Experience;
-import io.connectvitae.contracts.models.GeneralProfile;
-import io.connectvitae.contracts.models.Language;
-import io.connectvitae.contracts.models.Skill;
-import io.connectvitae.contracts.models.User;
+import io.connectvitae.contracts.models.*;
 import io.connectvitae.core.config.LinkedInProperties;
 import io.connectvitae.core.mappers.SeleniumMapperService;
 import io.connectvitae.seleniumProvider.services.SeleniumExtractorService;
 import io.connectvitae.seleniumProvider.services.SeleniumFetcherService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,7 +17,6 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/selenium")
-//TODO: Handle UnsupportedOperationException
 public class SeleniumController {
   private final SeleniumExtractorService extractorService;
   private final LinkedInProperties linkedInProperties;
@@ -31,62 +24,103 @@ public class SeleniumController {
   private final SeleniumMapperService mapperService;
   private boolean isAuthenticated = false;
 
-
+  @Operation(summary = "Get profile information")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Successfully retrieved profile"),
+          @ApiResponse(responseCode = "404", description = "Profile not found"),
+          @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
   @GetMapping("/profile/{profileId}")
   public GeneralProfile getProfile(@PathVariable String profileId) {
     authenticate();
     return mapperService.mapGeneralProfile(extractorService.getProfile(profileId));
   }
 
+  @Operation(summary = "Get user information")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Successfully retrieved user"),
+          @ApiResponse(responseCode = "404", description = "User not found"),
+          @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
   @GetMapping("/profile/{profileId}/user")
   public User getUser(@PathVariable String profileId) {
     authenticate();
     return mapperService.mapUser(extractorService.getUser(profileId));
   }
 
+  @Operation(summary = "Get experiences")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Successfully retrieved experiences"),
+          @ApiResponse(responseCode = "404", description = "Experiences not found"),
+          @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
   @GetMapping("/profile/{profileId}/experiences")
   public List<Experience> getExperiences(@PathVariable String profileId) {
     authenticate();
     return extractorService.getExperiences(profileId)
-        .stream()
-        .map(mapperService::mapExperience)
-        .collect(Collectors.toList());
+            .stream()
+            .map(mapperService::mapExperience)
+            .collect(Collectors.toList());
   }
 
+  @Operation(summary = "Get certifications")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Successfully retrieved certifications"),
+          @ApiResponse(responseCode = "404", description = "Certifications not found"),
+          @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
   @GetMapping("/profile/{profileId}/certifications")
   public List<Certification> getCertifications(@PathVariable String profileId) {
     authenticate();
     return extractorService.getCertifications(profileId)
-        .stream()
-        .map(mapperService::mapCertification)
-        .collect(Collectors.toList());
+            .stream()
+            .map(mapperService::mapCertification)
+            .collect(Collectors.toList());
   }
 
+  @Operation(summary = "Get educations")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Successfully retrieved educations"),
+          @ApiResponse(responseCode = "404", description = "Educations not found"),
+          @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
   @GetMapping("/profile/{profileId}/educations")
   public List<Education> getEducations(@PathVariable String profileId) {
     authenticate();
     return extractorService.getEducations(profileId)
-        .stream()
-        .map(mapperService::mapEducation)
-        .collect(Collectors.toList());
+            .stream()
+            .map(mapperService::mapEducation)
+            .collect(Collectors.toList());
   }
 
+  @Operation(summary = "Get skills")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Successfully retrieved skills"),
+          @ApiResponse(responseCode = "404", description = "Skills not found"),
+          @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
   @GetMapping("/profile/{profileId}/skills")
   public List<Skill> getSkills(@PathVariable String profileId) {
     authenticate();
     return extractorService.getSkills(profileId)
-        .stream()
-        .map(mapperService::mapSkill)
-        .collect(Collectors.toList());
+            .stream()
+            .map(mapperService::mapSkill)
+            .collect(Collectors.toList());
   }
 
+  @Operation(summary = "Get languages")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Successfully retrieved languages"),
+          @ApiResponse(responseCode = "404", description = "Languages not found"),
+          @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
   @GetMapping("/profile/{profileId}/languages")
   public List<Language> getLanguages(@PathVariable String profileId) {
     authenticate();
     return extractorService.getLanguages(profileId)
-        .stream()
-        .map(mapperService::mapLanguage)
-        .collect(Collectors.toList());
+            .stream()
+            .map(mapperService::mapLanguage)
+            .collect(Collectors.toList());
   }
 
   private void authenticate() {
--- connectvitae-core/src/main/java/io/connectvitae/core/controllers/VoyagerApiController.java
+++ connectvitae-core/src/main/java/io/connectvitae/core/controllers/VoyagerApiController.java
@@ -1,23 +1,15 @@
 package io.connectvitae.core.controllers;
 
-
-import io.connectvitae.contracts.models.Certification;
-import io.connectvitae.contracts.models.Company;
-import io.connectvitae.contracts.models.Education;
-import io.connectvitae.contracts.models.Experience;
-import io.connectvitae.contracts.models.GeneralProfile;
-import io.connectvitae.contracts.models.School;
-import io.connectvitae.contracts.models.Skill;
-import io.connectvitae.contracts.models.User;
+import io.connectvitae.contracts.models.*;
 import io.connectvitae.core.config.LinkedInProperties;
 import io.connectvitae.core.mappers.VoyagerApiMapperService;
 import io.connectvitae.voyagerApiProvider.services.VoyagerApiExtractorService;
 import io.connectvitae.voyagerApiProvider.services.VoyagerApiFetcherService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,72 +17,111 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/linkedIn")
-//TODO: Handle UnsupportedOperationException
 public class VoyagerApiController {
   private final LinkedInProperties linkedInProperties;
   private final VoyagerApiFetcherService fetcherService;
   private final VoyagerApiExtractorService extractorService;
   private final VoyagerApiMapperService mapperService;
-
   private boolean isAuthenticated = false;
+
+  @Operation(summary = "Get general profile by Linkedin ID")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "The profile is found"),
+          @ApiResponse(responseCode = "404", description = "Profile not found")
+  })
   @GetMapping("/{profileId}")
   public GeneralProfile getProfile(@PathVariable String profileId) {
     authenticate();
     return mapperService.mapGeneralProfile(extractorService.getProfile(profileId));
   }
 
+  @Operation(summary = "Get user details by LinkedIn ID")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "The user details are found"),
+          @ApiResponse(responseCode = "404", description = "User details not found")
+  })
   @GetMapping("/profile/{profileId}/user")
   public User getUser(@PathVariable String profileId) {
     authenticate();
     return mapperService.mapUser(extractorService.getUser(profileId));
   }
 
+  @Operation(summary = "Get experiences by LinkedIn ID")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Experiences are found "),
+          @ApiResponse(responseCode = "404", description = "Experiences not found")
+  })
   @GetMapping("/profile/{profileId}/experiences")
   public List<Experience> getExperiences(@PathVariable String profileId) {
     authenticate();
     return extractorService.getExperiences(profileId)
-        .stream()
-        .map(mapperService::mapExperience)
-        .collect(Collectors.toList());
+            .stream()
+            .map(mapperService::mapExperience)
+            .collect(Collectors.toList());
   }
 
+  @Operation(summary = "Get certifications by LinkedIn ID")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Certifications are found"),
+          @ApiResponse(responseCode = "404", description = "Certifications not found")
+  })
   @GetMapping("/profile/{profileId}/certifications")
   public List<Certification> getCertifications(@PathVariable String profileId) {
     authenticate();
     return extractorService.getCertifications(profileId)
-        .stream()
-        .map(mapperService::mapCertification)
-        .collect(Collectors.toList());
+            .stream()
+            .map(mapperService::mapCertification)
+            .collect(Collectors.toList());
   }
 
+  @Operation(summary = "Get educations by LinkedIn ID")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Educations are Found"),
+          @ApiResponse(responseCode = "404", description = "Educations not found")
+  })
   @GetMapping("/profile/{profileId}/educations")
   public List<Education> getEducations(@PathVariable String profileId) {
     authenticate();
     return extractorService.getEducations(profileId)
-        .stream()
-        .map(mapperService::mapEducation)
-        .collect(Collectors.toList());
+            .stream()
+            .map(mapperService::mapEducation)
+            .collect(Collectors.toList());
   }
 
+  @Operation(summary = "Get skills by LinkedIn ID")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Skills are found"),
+          @ApiResponse(responseCode = "404", description = "Skills not found")
+  })
   @GetMapping("/profile/{profileId}/skills")
   public List<Skill> getSkills(@PathVariable String profileId) {
     authenticate();
     return extractorService.getSkills(profileId)
-        .stream()
-        .map(mapperService::mapSkill)
-        .collect(Collectors.toList());
+            .stream()
+            .map(mapperService::mapSkill)
+            .collect(Collectors.toList());
   }
 
+  @Operation(summary = "Get company details by company ID")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Company details are found"),
+          @ApiResponse(responseCode = "404", description = "Company details not found")
+  })
   @GetMapping("/company/{companyId}")
   public List<Company> getCompany(@PathVariable String companyId) {
     authenticate();
     return extractorService.getCompany(companyId)
-        .getElements()
-        .stream()
-        .map(mapperService::mapCompany)
-        .toList();
+            .getElements()
+            .stream()
+            .map(mapperService::mapCompany)
+            .collect(Collectors.toList());
   }
 
+  @Operation(summary = "Get school details by school ID")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "School details are found"),
+          @ApiResponse(responseCode = "404", description = "School details not found")
+  })
   @GetMapping("/school/{schoolId}")
   public School getSchool(@PathVariable String schoolId) {
     authenticate();
